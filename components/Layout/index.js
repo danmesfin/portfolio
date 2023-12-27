e82d847d1cdf9294527629ac3c9bdff0c28795ae
danmesfin <danielmsfn@gmail.com>
2023-12-27 21:53:56 +0300

    integrate vercel speed insights

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,30 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import Head from 'next/head';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import Navbar from './Navbar';
 import Footer from './Footer';
-import CustomCursor from '../CustomCursor/index';
-
+// import CustomCursor from '../CustomCursor/index';
 // eslint-disable-next-line react/prop-types
 function Layout({ children }) {
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  // const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
-  useEffect(() => {
-    const handleMouseMove = (event) => {
-      setMousePosition({ x: event.clientX, y: event.clientY });
-    };
+  // useEffect(() => {
+  //   const handleMouseMove = (event) => {
+  //     setMousePosition({ x: event.clientX, y: event.clientY });
+  //   };
 
-    window.addEventListener('mousemove', handleMouseMove);
+  //   window.addEventListener('mousemove', handleMouseMove);
 
-    return () => {
-      window.removeEventListener('mousemove', handleMouseMove);
-    };
-  }, []);
+  //   return () => {
+  //     window.removeEventListener('mousemove', handleMouseMove);
+  //   };
+  // }, []);
   return (
     <main className=" bg-base dark:bg-black snap-y h-screen overflow-y-scroll scroll-smooth">
       {/* <CustomCursor mousePosition={mousePosition} /> */}
-
       <Navbar />
       {children}
+      <SpeedInsights />
       <Footer />
     </main>
   );
