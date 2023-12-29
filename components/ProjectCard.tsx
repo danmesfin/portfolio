98bc9cb8dd878dca5655ba8ddd40fce8782672f5
danmesfin <danielmsfn@gmail.com>
2023-12-29 17:36:50 +0300

    updated projects card

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -17,8 +17,14 @@ interface ProjectCardProps {
 const ProjectCard: React.FC<ProjectCardProps> = ({ project, onClick }) => (
   <Parallax translateY={[-10, 10]} onClick={onClick}>
     <a href={project.projecturl} target="_blank" rel="noreferrer">
-      <div className="p-5 cursor-pointer transition transform duration-300 hover:scale-105 border border-base">
-        <div className="relative w-60 h-60 md:h-72 md:w-72 lg:w-96 lg:h-96 shadow-md shadow-gray-600 dark:shadow-gray-900">
+      <div
+        className="p-5 cursor-pointer transition transform delay-75 duration-300 
+      hover:scale-[1.01] bg-gray-200 rounded-lg dark:bg-zinc-600 dark:bg-opacity-80 bg-opacity-90 "
+      >
+        <div
+          className="relative w-60 h-60 md:h-72 md:w-72 lg:w-96 lg:h-96 
+         dark:shadow-gray-900"
+        >
           <Image
             src={project.imgurl}
             alt={project.title}
@@ -26,11 +32,11 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project, onClick }) => (
             style={{ objectFit: 'contain' }}
             blurDataURL="data:..."
             placeholder="blur"
-            className="transform delay-150 duration-75 grayscale hover:grayscale-0 "
+            className="transform rounded-md delay-75 duration-75  "
           />
         </div>
 
-        <div className=" mt-14 md:mt-24 text-center text-base font-display">
+        <div className="mt-14 md:mt-16 text-center text-orangish dark:text-white font-display">
           <h3 className="text-lg ">{project.title}</h3>
           <p className="text-sm">{project.role}</p>
         </div>
