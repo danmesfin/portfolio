725d4714890bc54e7030876450c99c7f919ece65
danmesfin <danielmsfn@gmail.com>
2024-08-15 21:00:13 +0300

    🆕 feat(master) extend translate options

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,15 @@ module.exports = {
         base: '#bc3908',
         sky: '#941b0c',
       },
+      keyframes: {
+        'slide-up': {
+          '0%': { transform: 'translateY(100%)' },
+          '100%': { transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        'slide-up': 'slide-up 0.3s ease-out forwards',
+      },
     },
   },
   plugins: [],
