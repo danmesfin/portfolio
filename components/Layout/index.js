57187ca908e3a10f655665d96edc05773e69dc1f
danmesfin <danielmsfn@gmail.com>
2024-11-27 11:27:02 +0300

    refactor on contacts page

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import Navbar from './Navbar';
